779761cbdbd7d8d5b706926f81add4b780a5e001
Igor Nascimento <igornascimentodesouza@gmail.com>
2023-07-16 16:45:07 -0400

    adicionando o filtro de data

--- src/components/Transferencias.js
+++ src/components/Transferencias.js
@@ -1,38 +1,92 @@
 import { useEffect } from 'react';
 import { useState } from "react";
+import moment from 'moment/moment';
 
-function Transferencias(){
+function Transferencias() {
 
-  const [dados, setDados] = useState([]);
+  const API = 'http://localhost:8080/v1/transferencias';
+
+  const [filtroDataInicio, setFiltroDataInicio] = useState('');
+  const [filtroDataFim, setFiltroDataFim] = useState('');
+  const [dadosFiltrados, setDadosFiltrados] = useState(null);
 
 
   useEffect(() => {
-    fetch('http://localhost:8080/v1/transferencias')
-      .then(response => response.json())
-      .then(data => setDados(data));
-  }, []);
+    const fetchData = async () => {
+      let queryParams = '/';
+
+      if (filtroDataInicio) {
+        const dataFormatada = moment(filtroDataInicio, 'DD/MM/YYYY').format('YYYY-MM-DDTHH:mm:ss');
+        queryParams += `${dataFormatada}/`;
+      }
+
+      if (filtroDataFim) {
+        const dataFormatada = moment(filtroDataFim, 'DD/MM/YYYY').format('YYYY-MM-DDTHH:mm:ss');
+        queryParams += `${dataFormatada}/`;
+      }
+
+      try {
+        const response = await fetch(API + `${queryParams}`);
+        const data = await response.json();
+
+        if (!Array.isArray(data)){
+          setDadosFiltrados([data]);
+        } else {
+          setDadosFiltrados(data);
+        }
+
+        
+      } catch (error) {
+        console.error('Erro ao buscar os dados da API:', error);
+      }
+    };
+
+    fetchData();
+  }, [filtroDataInicio, filtroDataFim]);
+
+  if (dadosFiltrados === null) {
+    return <div>Carregando...</div>; // Exibe uma mensagem de carregamento enquanto os dados são obtidos
+  }
+
+
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Coluna 1</th>
-          <th>Coluna 2</th>
-          <th>Coluna 3</th>
-          <th>Coluna 4</th>
-        </tr>
-      </thead>
-      <tbody>
-        {dados.map(item => (
-          <tr key={item.id}>
-            <td>{item.dataTransferencia}</td>
-            <td>{item.valor}</td>
-            <td>{item.tipo}</td>
-            <td>{item.nomeOperadorTransacao}</td>
+
+    <div>
+      <input
+        type="text"
+        value={filtroDataInicio}
+        onChange={e => setFiltroDataInicio(e.target.value)}
+        placeholder="Data inicial"
+      />
+      <input
+        type="text"
+        value={filtroDataFim}
+        onChange={e => setFiltroDataFim(e.target.value)}
+        placeholder="Data final"
+      />
+
+      <table>
+        <thead>
+          <tr>
+            <th>Coluna 1</th>
+            <th>Coluna 2</th>
+            <th>Coluna 3</th>
+            <th>Coluna 4</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {dadosFiltrados.map(item => (
+            <tr key={item.id}>
+              <td>{item.dataTransferencia}</td>
+              <td>{item.valor}</td>
+              <td>{item.tipo}</td>
+              <td>{item.nomeOperadorTransacao}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
